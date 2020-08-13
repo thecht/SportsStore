0d9f8a4fec2b92f9a57dbcb69785e15ff08fb9de
Theodore Hecht <thecht182@gmail.com>
2020-08-13 18:20:47 -0400

    Initial project.

---
+++ SportsStoreMVC/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using SportsStoreMVC.Models;
+using System.Linq;
+
+namespace SportsStoreMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public int PageSize = 4;
+
+        private IStoreRepository repository;
+
+        public HomeController(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+
+        public IActionResult Index(int productPage = 1)
+        {
+            var products = repository.Products
+                            .OrderBy(p => p.ProductID)
+                            .Skip((productPage - 1) * PageSize)
+                            .Take(PageSize);
+
+            return View(products);
+        }
+    }
+}
---
+++ SportsStoreMVC/Models/EFStoreRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStoreMVC.Models
+{
+    public class EFStoreRepository : IStoreRepository
+    {
+        public IQueryable<Product> Products => context.Products;
+
+        private StoreDbContext context;
+
+        public EFStoreRepository(StoreDbContext ctx)
+        {
+            context = ctx;
+        }
+
+    }
+}
