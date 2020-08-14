f544bc7f2655827a11349597ddf9a365d33ce8e7
Theodore Hecht <thecht182@gmail.com>
2020-08-14 01:56:38 -0400

    Adds properties to PageLinkTagHelper for more modular styling, and restyles product list css.

--- SportsStoreMVC/Infrastructure/PageLinkTagHelper.cs
+++ SportsStoreMVC/Infrastructure/PageLinkTagHelper.cs
@@ -29,6 +29,11 @@ namespace SportsStoreMVC.Infrastructure
 
         public string PageAction { get; set; }
 
+        public bool PageClassesEnabled { get; set; } = false;
+        public string PageClass { get; set; }
+        public string PageClassNormal { get; set; }
+        public string PageClassSelected { get; set; }
+
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
@@ -38,6 +43,12 @@ namespace SportsStoreMVC.Infrastructure
             {
                 TagBuilder tag = new TagBuilder("a");
                 tag.Attributes["href"] = urlHelper.Action(PageAction, new { productPage = i });
+                if (PageClassesEnabled)
+                {
+                    tag.AddCssClass(PageClass);
+                    tag.AddCssClass(i == PageModel.CurrentPage ? PageClassSelected : PageClassNormal);
+                }
+                
                 tag.InnerHtml.Append(i.ToString());
                 result.InnerHtml.AppendHtml(tag);
             }
