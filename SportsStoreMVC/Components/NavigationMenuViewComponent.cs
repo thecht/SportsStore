46fa8e141247f6ba0d70199a5d9afcb5251baf2a
Theodore Hecht <thecht182@gmail.com>
2020-08-14 21:20:19 -0400

    Adds category navigation sidebar.

---
+++ SportsStoreMVC/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStoreMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStoreMVC.Components
+{
+    public class NavigationMenuViewComponent: ViewComponent
+    {
+        private IStoreRepository repository;
+
+        public NavigationMenuViewComponent(IStoreRepository repo)
+        {
+            repository = repo;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            var productsByCategory = repository.Products
+                                               .Select(x => x.Category)
+                                               .Distinct()
+                                               .OrderBy(x => x);        
+            
+            return View(productsByCategory);            
+        }
+
+    }
+}
