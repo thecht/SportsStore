99ffec06e0934c992013fac8a9f692ad552e6170
Theodore Hecht <thecht182@gmail.com>
2020-08-19 16:35:25 -0400

    Add order form.

---
+++ SportsStoreMVC/Controllers/OrderController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStoreMVC.Models;
+
+namespace SportsStoreMVC.Controllers
+{
+    public class OrderController : Controller
+    {
+        public ViewResult Checkout()
+        {
+            return View(new Order());
+        }
+    }
+}
