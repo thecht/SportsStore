561b223c47c27ca8fdde0f3910b8c432f00f095a
Theodore Hecht <thecht182@gmail.com>
2020-08-19 03:57:27 -0400

    Adds cart icon while browsing.

---
+++ SportsStoreMVC/Components/CartSummaryViewComponent.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStoreMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStoreMVC.Components
+{
+    public class CartSummaryViewComponent: ViewComponent
+    {
+        private Cart cart;
+
+        public CartSummaryViewComponent(Cart cartService)
+        {
+            cart = cartService;
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return View(cart);
+        }
+    }
+}
