05475223041c530b9c9a352e819b40b6723a95ce
Theodore Hecht <thecht182@gmail.com>
2020-08-27 00:26:04 -0400

    Adds model validation.

--- SportsStoreMVC/Models/Product.cs
+++ SportsStoreMVC/Models/Product.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
@@ -9,12 +10,18 @@ namespace SportsStoreMVC.Models
     public class Product
     {
         public long ProductID { get; set; }
+
+        [Required(ErrorMessage = "Please enter a product name")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "Please enter a description")]
         public string Description { get; set; }
 
-        [Column(TypeName = "decimal(8, 2)")]
+        [Required]
+        [Range(0.01, double.MaxValue, ErrorMessage = "Please enter a positive price")]
         public decimal Price { get; set; }
 
+        [Required(ErrorMessage = "Please specify a category")]
         public string Category { get; set; }
     }
 }
